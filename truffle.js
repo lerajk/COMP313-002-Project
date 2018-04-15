8b69316e959200bf2ecbad7ad4823fc84a737a85
Lee Raj <lerajk44@gmail.com>
2018-04-15 17:10:29 -0400

    final updates

--- truffle.js
+++ truffle.js
@@ -1,20 +1,30 @@
-
-var HDWalletProvider = require("truffle-hdwallet-provider");
+var HDWalletProvider = require('truffle-hdwallet-provider');
+var mnemonic = '';
 require('babel-register');
 require('babel-polyfill');
-var mnemonic = "";
+
 module.exports = {
-  networks : {
-    development: {
-      host: "localhost",
-      port: "8545",
-      network_id: "*"
-    },
-    ropsten: {
-     provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/"),
-     network_id: 3
-   }
-  }
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+    networks: {
+    development: {
+      host: 'localhost',
+      port: '8545',
+      network_id: '*',
+    },
+
+    ropsten: {
+     provider: new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/secret'),
+     network_id: 3,
+     gas: 4612388
+    },
+    
+    coverage: {
+      host: "localhost",
+      network_id: "*",
+      port: 8555,         // <-- If you change this, also set the port option in .solcover.js.
+      gas: 0xfffffffffff, // <-- Use this high gas value
+      gasPrice: 0x01      // <-- Use this low gas price
+    }
+ },
 };
